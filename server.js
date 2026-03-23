1e3811fcde86c5001ee07340aa71b66f78a088ff
scriptedbyfazil <25bcyb18@kristujayanti.com>
2026-03-23 23:15:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "mysql.railway.internal",
   user: "root",
-  password: "root", 
-  database: "eventdb"
+  password: "xZBAvkGqrfRbfYQXNBURZhyyRSTjvYoS",
+  database: "railway",
+  port: YOUR_PORT
 });
 
 db.connect((err) => {
@@ -19,6 +20,22 @@ db.connect((err) => {
     console.error("Database connection failed:", err);
   } else {
     console.log("Connected to MySQL database");
+
+    db.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(100),
+        phone VARCHAR(20),
+        ticket VARCHAR(50)
+      )
+    `, (err) => {
+      if (err) {
+        console.error("Error creating table:", err);
+      } else {
+        console.log("Users table ready");
+      }
+    });
   }
 });
 
