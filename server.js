f66a7e5a337087703caf69a8c648f8f4b9b8af1b
scriptedbyfazil <25bcyb18@kristujayanti.com>
2026-03-23 23:47:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const db = mysql.createConnection({
   user: "root",
   password: "xZBAvkGqrfRbfYQXNBURZhyyRSTjvYoS",
   database: "railway",
-  port: 3306
+  port: 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
