7e4b15a3bef695bee19476232a6a6760a0b82f57
scriptedbyfazil <25bcyb18@kristujayanti.com>
2026-03-23 22:50:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", async function (e) {
   };
 
   try {
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("https://event-registration-backend-lzh9.onrender.com/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
